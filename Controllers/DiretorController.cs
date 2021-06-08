90aabb371364d58b310af49e44d5dca7315fde58
jessica bohn <jessicabohn74@gmail.com>
2021-06-07 21:42:12 -0300

    Create DiretorController.cs

---
+++ Controllers/DiretorController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+[ApiController]
+[Route("[controller]")]
+public class DiretorController: ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+    
+    public DiretorController(ApplicationDbContext context) 
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<List<Diretor>> Get()
+    {
+        return await _context.Diretores.ToListAsync();
+    } 
+
+    [HttpPost]
+    public async Task<ActionResult<Diretor>> Post([FromBody] Diretor diretor) {
+        _context.Diretores.Add(diretor);
+        await _context.SaveChangesAsync();
+
+        return Ok(diretor);
+    }
+
+    [HttpPut]
+    public string Put()
+    {
+        return "Put";
+    }
+
+
+    [HttpDelete]
+    public string Delete()
+    {
+        return "Delete";
+    }
+}
