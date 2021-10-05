10f56ffad053b54c583b2d99dc87be667f577afb
jessica bohn <jessicabohn74@gmail.com>
2021-10-04 22:49:30 -0300

    modificação do HelloWorld

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,12 +1,1 @@
-﻿using System;
-
-namespace HelloWorld
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+﻿System.Console.WriteLine("Hello World!");
